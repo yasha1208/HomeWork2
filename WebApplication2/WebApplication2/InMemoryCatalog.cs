2db536bb21f6d23ee3064a013ae115ae7b3089cc
yasha1208 <68597943+yasha1208@users.noreply.github.com>
2021-11-28 09:46:25 +0300

    Add files via upload

---
+++ WebApplication2/WebApplication2/InMemoryCatalog.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication2;
+
+public class InMemoryCatalog : ICatalog
+{
+    private IReadOnlyCollection<Product> _products = new Product[]
+    {
+        new (Name:"Книга 1", Price:100m),
+        new (Name:"Книга 1", Price:200m),
+        new (Name:"Книга 1", Price:300m),
+    };
+
+    public IReadOnlyCollection<Product> GetProducts()
+    {
+        if (DateTime.UtcNow.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday)
+        {
+            return _products.Select(it => new Product(it.Name, 
+                it.Price * 1.5m)).ToList();
+        }
+
+        return _products;
+    } 
+}
